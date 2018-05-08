ec977f7048107ef349608a8612f88bd52a9435e6
Tomas <tmgarcia@miuandes.cl>
2018-05-08 16:48:13 -0300

    Act

--- LabPOO/Program.cs
+++ LabPOO/Program.cs
@@ -7,16 +7,25 @@ using System.Threading.Tasks;
 
 namespace LabPOO
 {
+    public delegate void bigSister(List<Product> list1);
+
+
     class Program
     {
         public static List<Product> cart;
         public static List<Product> market;
 
+        public event bigSister BigCheck;
+
         static void Main(string[] args)
         {
+            
             cart = new List<Product>();
             market = new List<Product>();
+            //BigCheck += new bigSister(List<Product> cart);
+
             SupplyStore();
+
             while (true)
             {
                 PrintHeader();
@@ -57,6 +66,87 @@ namespace LabPOO
             }
         }
 
+        protected void onBigCheck(List<Product> cart3)
+        {
+            BigCheck.Invoke(cart3);
+            int can = cart3.Count();
+            cart3.RemoveAt(can - 1);
+        }
+        
+        public static void BigsisCheck(List<Product> cart2)
+        {
+            int can = cart2.Count();
+            cart2.RemoveAt(can - 1);
+        }
+
+        public static void Revisar(int answer, List<Product> cart1)
+        {
+
+            switch (answer)
+            {
+                case 1:
+                    {
+                        BigsisCheck(cart1);
+                        break; 
+                    }
+                case 3:
+                    {
+                        BigsisCheck(cart1);
+                        break;
+                    }
+                case 5:
+                    {
+                        BigsisCheck(cart1);
+                        break;
+                    }
+                case 7:
+                    {
+                        int indice = cart1.Count();
+                        cart1.RemoveAt(indice - 1);
+                        break;
+                    }
+                case 9:
+                    {
+                        int indice = cart1.Count();
+                        cart1.RemoveAt(indice - 1);
+                        break;
+                    }
+                case 11:
+                    {
+                        int indice = cart1.Count();
+                        cart1.RemoveAt(indice - 1);
+                        break;
+                    }
+                case 12:
+                    {
+                        int indice = cart1.Count();
+                        cart1.RemoveAt(indice - 1);
+                        break;
+                    }
+                case 15:
+                    {
+                        int indice = cart1.Count();
+                        cart1.RemoveAt(indice - 1);
+                        break;
+                    }
+                case 16:
+                    {
+                        int indice = cart1.Count();
+                        cart1.RemoveAt(indice - 1);
+                        break;
+                    }
+                case 21:
+                    {
+                        int indice = cart1.Count();
+                        cart1.RemoveAt(indice - 1);
+                        break;
+                    }
+            }
+        }
+
+
+
+
         public static void Pay()
         {
             PrintHeader();
@@ -93,6 +183,7 @@ namespace LabPOO
                         continue;
                     }
                     AddToCart(market[answer]);
+                    Revisar(answer, cart);
                     break;
                 }
                 catch
@@ -136,29 +227,29 @@ namespace LabPOO
 
         public static void SupplyStore()
         {
-            market.Add(new Product("Leche Entera", 820, 89, "1L"));
-            market.Add(new Product("Gomitas Flipy", 720, 12, "100g"));
-            market.Add(new Product("Mantequilla", 850, 12, "125g"));
-            market.Add(new Product("Crema para hemorroides", 4990, 7, "300cc"));
-            market.Add(new Product("Pimienta", 430, 84, "15g"));
-            market.Add(new Product("Vino Sauvignon Blanc Reserva Botella", 4150, 23, "750cc"));
-            market.Add(new Product("Sal Lobos", 330, 150, "1kg"));
-            market.Add(new Product("Cuaderno Mi Pequeño Pony", 1290, 50, "1un"));
-            market.Add(new Product("Láminas de Lasaña", 1250, 85, "400g"));
-            market.Add(new Product("Tomate", 1290, 200, "1kg"));
-            market.Add(new Product("Harina", 890, 43, "1kg"));
-            market.Add(new Product("Audifonos Samsung", 5990, 40, "1un"));
-            market.Add(new Product("Pisco Alto del Carmen", 5990, 120, "1L"));
-            market.Add(new Product("Carne Molida", 4390, 15, "500g"));
-            market.Add(new Product("Aceite de Oliva", 1790, 77, "250g"));
-            market.Add(new Product("Sal parrillera", 840, 50, "750g"));
-            market.Add(new Product("Cable HDMI 1m", 3990, 25, "1un"));
-            market.Add(new Product("Queso Rallado Parmesano", 499, 102, "40g"));
-            market.Add(new Product("Vino Blanco Caja", 2790, 84, "2L"));
-            market.Add(new Product("Malla de Cebollas", 1090, 91, "1kg"));
-            market.Add(new Product("Tomates Pelados en lata", 700, 48, "540g"));
-            market.Add(new Product("Queso Parmesano", 3790, 41, "200g"));
-            market.Add(new Product("Bolsa de Zanahorias", 890, 74, "1un"));
+            market.Add(new Product("Leche Entera", 820, 89, "1L"));//si 0
+            market.Add(new Product("Gomitas Flipy", 720, 12, "100g")); //no va 1
+            market.Add(new Product("Mantequilla", 850, 12, "125g"));//si 2
+            market.Add(new Product("Crema para hemorroides", 4990, 7, "300cc")); //no va 3
+            market.Add(new Product("Pimienta", 430, 84, "15g"));//si 4
+            market.Add(new Product("Vino Sauvignon Blanc Reserva Botella", 4150, 23, "750cc")); //no va 5
+            market.Add(new Product("Sal Lobos", 330, 150, "1kg")); //si 6
+            market.Add(new Product("Cuaderno Mi Pequeño Pony", 1290, 50, "1un")); //no va 7
+            market.Add(new Product("Láminas de Lasaña", 1250, 85, "400g"));//si 8
+            market.Add(new Product("Tomate", 1290, 200, "1kg"));//no va 9
+            market.Add(new Product("Harina", 890, 43, "1kg"));//si 10
+            market.Add(new Product("Audifonos Samsung", 5990, 40, "1un")); //no va11
+            market.Add(new Product("Pisco Alto del Carmen", 5990, 120, "1L")); //no va 12
+            market.Add(new Product("Carne Molida", 4390, 15, "500g")); //si 13
+            market.Add(new Product("Aceite de Oliva", 1790, 77, "250g"));//si 14
+            market.Add(new Product("Sal parrillera", 840, 50, "750g"));//no va 15 
+            market.Add(new Product("Cable HDMI 1m", 3990, 25, "1un"));//no va 16
+            market.Add(new Product("Queso Rallado Parmesano", 499, 102, "40g"));//si 17
+            market.Add(new Product("Vino Blanco Caja", 2790, 84, "2L"));//si 18
+            market.Add(new Product("Malla de Cebollas", 1090, 91, "1kg"));//si 19
+            market.Add(new Product("Tomates Pelados en lata", 700, 48, "540g"));//si 20
+            market.Add(new Product("Queso Parmesano", 3790, 41, "200g")); //no va 21
+            market.Add(new Product("Bolsa de Zanahorias", 890, 74, "1un"));//si 22
         }
 
         public static void ShowRecipe()
